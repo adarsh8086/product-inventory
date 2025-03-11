a7a4af5a7db17b4898ce8574c900517178ce3055
adarsh m k <adarshmk882@gmail.com>
2025-03-11 21:21:39 +0530

    updated urls.py and other some files

--- inventory_api/product/forms.py
+++
@@ -1,8 +0,0 @@
-
-from django import forms
-from .models import Product
-
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = ['name', 'price', 'stock']  # Include the fields you want to manage in the form
